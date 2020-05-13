e727851e6a6291e44fdee9701317c5f6ae5f1547
xiaozwu2018 <xiaozwu2018@gmail.com>
2020-05-13 12:08:30 -0700

    Complete module

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("\")
-@app.route("\jobs")
+@app.route("/")
+
+@app.route("/jobs")
+
 def jobs():
     return render_template('index.html')
