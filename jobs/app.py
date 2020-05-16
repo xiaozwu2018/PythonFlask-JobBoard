3ebdeef0584c6af05331bdb6d03576fa84adc9a6
Xiaozhen <xiaozwu2018@gmail.com>
2020-05-16 15:09:55 -0700

    module5 complete

--- jobs/app.py
+++ jobs/app.py
@@ -40,3 +40,13 @@ def jobs():
                 'JOIN employer ON employer.id = job.employer_id')
 
     return render_template('index.html', jobs=jobs)
+
+
+@app.rout("job/<job_id>")
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id '
+                'as employer_id, employer.name as employer_name FROM job '
+                'JOIN employer ON employer.id = job.employer_id WHERE job.id=?',
+                [job_id], single=True)
+
+    return render_template("job.html", job=job)
